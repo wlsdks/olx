58a61951d918bb03e1c714635380919a5333b2d6
choijinan <wlsdks12@naver.com>
2022-02-17 00:07:53 +0900

    sss

--- olx/src/main/java/com/olx/OlxApplication.java
+++ olx/src/main/java/com/olx/OlxApplication.java
@@ -1,8 +1,18 @@
 package com.olx;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+import com.olx.dao.IChatDAO;
+import com.olx.dao.IMemberDAO;
+import com.olx.dao.IPostDAO;
+
+@ComponentScan(basePackages = "com.project.finalProject")
+@MapperScan(basePackageClasses = IMemberDAO.class)
+@MapperScan(basePackageClasses = IPostDAO.class)
+@MapperScan(basePackageClasses = IChatDAO.class)
 @SpringBootApplication
 public class OlxApplication {
 
